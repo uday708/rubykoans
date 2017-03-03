0f862f22e9834d36853f684193cefafab4f4df9e
udprotocol <udaygupta708@gmail.com>
2017-03-03 19:08:34 +0530

    first commit

--- about_symbols.rb
+++ about_symbols.rb
@@ -75,8 +75,8 @@ class AboutSymbols < Neo::Koan
 
   def test_symbols_do_not_have_string_methods
     symbol = :not_a_string
-    assert_equal , symbol.respond_to?(:each_char)
-    assert_equal , symbol.respond_to?(:reverse)
+    assert_equal false, symbol.respond_to?(:each_char)
+    assert_equal false, symbol.respond_to?(:reverse)
   end
 
   # It's important to realize that symbols are not "immutable
@@ -85,13 +85,13 @@ class AboutSymbols < Neo::Koan
 
   def test_symbols_cannot_be_concatenated
     # Exceptions will be pondered further down the path
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       :cats + :dogs
     end
   end
 
   def test_symbols_can_be_dynamically_created
-    assert_equal __, ("cats" + "dogs").to_sym
+    assert_equal :catsdogs, ("cats" + "dogs").to_sym
   end
 
   # THINK ABOUT IT:
